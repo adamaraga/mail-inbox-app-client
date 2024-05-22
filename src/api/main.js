eeb389aebc6058186557789944a9ae8234ebbb9f
Adam Abdullahi Araga <adam@Adams-MacBook-Pro.local>
2024-05-22 02:04:39 +0100

    added remote api url update

--- src/api/main.js
+++ src/api/main.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-const apiURL = process.env.REACT_APP_API_URL;
+const apiURLMain = process.env.REACT_APP_API_URL;
 
 export const fetchUser = () => {
   return axios.get(`${process.env.REACT_APP_API_URL_USER}`);
 };
 
 export const fetchUserMessageCount = (id) => {
-  return axios.get(`${apiURL}/api/messages/count/${id}`);
+  return axios.get(`${apiURLMain}/api/messages/count/${id}`);
 };
 
 export const fetchUserMessages = (id) => {
-  return axios.get(`${apiURL}/api/messages/user/${id}`);
+  return axios.get(`${apiURLMain}/api/messages/user/${id}`);
 };
 
 export const fetchUserMessage = (id) => {
-  return axios.get(`${apiURL}/api/messages/${id}`);
+  return axios.get(`${apiURLMain}/api/messages/${id}`);
 };
 
 export const updateReadStatus = (id) => {
-  return axios.put(`${apiURL}/api/messages/read-update/${id}`, {
+  return axios.put(`${apiURLMain}/api/messages/read-update/${id}`, {
     isRead: "true",
   });
 };
